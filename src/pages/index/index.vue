1d0fcbb4852a7355bdd0af7cbd8532bbf815623c
Elton Zheng <elton.zheng.sh@gmail.com>
2022-04-21 21:39:15 +0800

    Completed onPullDownRefresh function.

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,10 @@
 <template>
   <view class="container m-4 space-y-5">
-    <view v-for="news in newsList" class="text-neutral-700">
+    <view
+      v-for="news in newsList"
+      :key="news.uniquekey"
+      class="text-neutral-700"
+    >
       <text class="block">{{ news.title }}</text>
       <image
         class="block mx-auto my-2"
@@ -15,19 +19,34 @@
 </template>
 
 <script setup>
-  import { reactive } from 'vue';
-  import { onLoad } from '@dcloudio/uni-app';
+  import { ref, reactive } from 'vue';
+  import { onLoad, onPullDownRefresh } from '@dcloudio/uni-app';
 
   const newsList = reactive([]);
-  onLoad(() => {
+  const page = ref(1);
+  const loadDate = (n) => {
     uni.request({
-      url: 'http://v.juhe.cn/toutiao/index?type=top&page_size=10&key=e7eb489fc2dd2a193e3c40b1b2f581e6',
+      url: `https://v.juhe.cn/toutiao/index?type=top&page=${n}&page_size=10&key=e7eb489fc2dd2a193e3c40b1b2f581e6`,
       success: (res) => {
         // console.log(res.data.result.data);
         newsList.push(...res.data.result.data);
         console.log(newsList);
       },
     });
+  };
+  onLoad(() => {
+    loadDate(page);
+  });
+
+  onPullDownRefresh(() => {
+    // 清空原有数据
+    newsList.length = 0;
+    page.value = 1;
+    loadDate(page);
+    console.log('refresh');
+    setTimeout(() => {
+      uni.stopPullDownRefresh();
+    }, 1000);
   });
 </script>
 
