cb81ff29f9c8dd67d1d780b3aa987058b5af6706
Elton Zheng <elton.zheng.sh@gmail.com>
2022-04-21 21:11:27 +0800

    Display latest 10 news in Index page.

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,12 +1,34 @@
 <template>
-  <view class="container">
-    <text class="text-4xl text-sky-600">{{ greeting }}</text>
+  <view class="container m-4 space-y-5">
+    <view v-for="news in newsList" class="text-neutral-700">
+      <text class="block">{{ news.title }}</text>
+      <image
+        class="block mx-auto my-2"
+        v-if="news.thumbnail_pic_s"
+        :src="news.thumbnail_pic_s"
+      />
+      <view class="flex justify-between text-sm text-neutral-500"
+        ><text>{{ news.author_name }}</text> <text>{{ news.date }}</text></view
+      >
+    </view>
   </view>
 </template>
 
 <script setup>
-  import { ref } from 'vue';
-  const greeting = ref('hello');
+  import { reactive } from 'vue';
+  import { onLoad } from '@dcloudio/uni-app';
+
+  const newsList = reactive([]);
+  onLoad(() => {
+    uni.request({
+      url: 'http://v.juhe.cn/toutiao/index?type=top&page_size=10&key=e7eb489fc2dd2a193e3c40b1b2f581e6',
+      success: (res) => {
+        // console.log(res.data.result.data);
+        newsList.push(...res.data.result.data);
+        console.log(newsList);
+      },
+    });
+  });
 </script>
 
 <style></style>
