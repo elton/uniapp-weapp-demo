8c992b20f0254650ea451415dab0d00b204b2e31
Elton Zheng <elton.zheng.sh@gmail.com>
2022-04-22 22:14:41 +0800

    Polish the detail page.

--- src/components/NewsItem/NewsItem.vue
+++ src/components/NewsItem/NewsItem.vue
@@ -6,14 +6,23 @@
     :src="news.thumbnail_pic_s"
   />
   <view class="flex justify-between text-sm text-neutral-500"
-    ><text>{{ news.author_name }}</text> <text>{{ news.date }}</text></view
+    ><text>{{ news.author_name }}</text> <text>{{ date }}</text></view
   >
 </template>
 
 <script setup>
+  import dayjs from 'dayjs';
+  import 'dayjs/locale/zh-cn';
+  import relativeTime from 'dayjs/plugin/relativeTime';
+  dayjs.extend(relativeTime);
+
+  dayjs.locale('zh-cn');
+
   const props = defineProps({
     news: Object,
   });
+
+  const date = dayjs(props.news.date).fromNow(true) + '前';
 </script>
 
 <style lang="scss" scoped></style>
--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -5,19 +5,28 @@
     </view>
     <view class="flex justify-between text-sm text-neutral-500 my-2">
       <text>{{ newsDetail?.data?.detail?.author_name }}</text>
-      <text>{{ newsDetail?.data?.detail?.date }}</text>
+      <text>{{
+        dayjs(newsDetail?.data?.detail?.date).fromNow(true) + '前'
+      }}</text>
     </view>
-    <view class="text-neutral-600">
-      <rich-text :nodes="newsDetail?.data?.content"></rich-text>
+    <view class="text-neutral-600 pb-6">
+      <rich-text :nodes="content?.data"></rich-text>
     </view>
   </view>
 </template>
 
 <script setup>
   import { onLoad } from '@dcloudio/uni-app';
-  import { reactive } from 'vue';
+  import { ref, reactive } from 'vue';
+  import dayjs from 'dayjs';
+  import 'dayjs/locale/zh-cn';
+  import relativeTime from 'dayjs/plugin/relativeTime';
+  dayjs.extend(relativeTime);
+
+  dayjs.locale('zh-cn');
 
   const newsDetail = reactive({ data: {} });
+  const content = ref({ data: '' });
 
   const getNewsDetail = (newsID) => {
     uni.showLoading({
@@ -26,8 +35,14 @@
     uni.request({
       url: `https://v.juhe.cn/toutiao/content?uniquekey=${newsID}&key=e7eb489fc2dd2a193e3c40b1b2f581e6`,
       success: (res) => {
-        // console.log(res.data.result);
+        // console.log(res.data.result.content);
         newsDetail.data = res.data.result;
+        // 设置正文样式
+        content.value = {
+          data: res.data.result.content
+            .replace(/\<img/gi, '<img style="margin: 0.5em 0"')
+            .replace(/\<p/gi, '<p style="margin: 0.75em 0"'),
+        };
         uni.hideLoading();
         // console.log(newsDetail.data);
       },
